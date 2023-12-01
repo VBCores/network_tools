4c1ea3d8d9ed62884226ee6f93cd3e8de151ac17
Igor Beschastnov <beschastnovigor@gmail.com>
2023-12-01 15:22:46 +0300

    small fixes

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
-def singleton(class_):
+from typing import Any, Callable, Type, TypeVar
+
+T = TypeVar("T")
+
+
+def singleton(class_: Type[T]) -> Callable[..., T]:
     instances = {}
 
-    def getinstance(*args, **kwargs):
+    def getinstance(*args: Any, **kwargs: Any) -> T:
         if class_ not in instances:
             instances[class_] = class_(*args, **kwargs)
         return instances[class_]
